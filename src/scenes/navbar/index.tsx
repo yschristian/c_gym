857dd2757b2cb31dcbf475dde8fddbf222a7baae
yschristian <yubahwesc@gmail.com>
2023-01-12 11:43:36 +0200

    navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,9 +1,56 @@
 import { useState } from "react"
-type Props = {}
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png"
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+type Props = {
+  selectedPage: SelectedPage,
+    setSelectedPage : (value: SelectedPage )=> void;
+}
 
-const Navbar = (props: Props) => {
+const Navbar = ({selectedPage , setSelectedPage}: Props) => {
+  const flexBetween = "flex items-center justify-between";
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6 `}>
+          <div className={`${flexBetween} w-full gap-16 `}>
+            {/* LEFT SIDE*/}
+            <img alt="logo" src={Logo} />
+            {/* RIGHT SIDE*/}
+            <div className={`${flexBetween} w-full `}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
   )
 }
 
